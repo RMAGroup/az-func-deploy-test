928186784b20121ed4e8158f320e8e5b4f212268
= <=>
2022-01-17 19:25:25 +0700

    Add skdService and codegen

--- ensureIncomingContainer/index.ts
+++ ensureIncomingContainer/index.ts
@@ -4,8 +4,8 @@ import { AzureBlobService } from "../shared/AzureBlobService";
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     const blobService = new AzureBlobService(process.env.AzureWebJobsStorage)
 
-    await blobService.getCreateContainer("hello-container")
-    context.log("created hello container")
+    await blobService.getCreateContainer("hello-container-2")
+    context.log("created hello container-2")
 
     
 };
